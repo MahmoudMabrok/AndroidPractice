b4006555de51720e947bd9bd6e8e5db1873b9149
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2019-12-30 09:14:13 +0200

    Update Car.java

--- app/src/main/java/com/mahmoudmabrok/androidfoundation/feature/dagger/Car.java
+++ app/src/main/java/com/mahmoudmabrok/androidfoundation/feature/dagger/Car.java
@@ -4,17 +4,11 @@ import javax.inject.Inject;
 
 public class Car {
 
-    private static final String TAG = "Car";
-  /*  @Inject
-    Engine engine;
-*/
-
     @Inject
     public Car() {
 
     }
 
     public void drive() {
-        //         Log.d(TAG, "drive: " + engine.toString());
     }
 }
